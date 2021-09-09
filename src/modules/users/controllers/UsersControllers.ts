50960d65c8c6eb63cb67e236cac87047ccdad40e
rafael.castanheira <42079091+rafacastanheira@users.noreply.github.com>
2021-09-09 01:45:42 -0300

    feat: added createUser with hashPassword (#2)
    * chore: github init
    
    * feat: added createUser with hashPassword

--- src/modules/users/controllers/UsersControllers.ts
+++ src/modules/users/controllers/UsersControllers.ts
@@ -1,7 +1,30 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
 
-export default class UsersController{
-    public createUsers(request: Request, response: Response): Response<string>{
-        return response.status(200).json('user was created')
-    }
+import CreateUserService from '../services/CreateUserService';
+
+import UsersRepository from '@infra/repositories/UsersRepository';
+
+import { classToClass } from 'class-transformer';
+
+export default class UsersController {
+  public async createUsers(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const { name, email, password, preferences, credit_card_id } = request.body;
+
+    const userRepository = new UsersRepository();
+
+    const createUserService = new CreateUserService(userRepository);
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+      preferences,
+      credit_card_id,
+    });
+
+    return response.status(200).json(classToClass(user));
+  }
 }
---
+++ src/modules/users/dtos/ICreateUserDTO.ts
@@ -0,0 +1,7 @@
+export default interface ICreateUserDto {
+  name: string;
+  email: string;
+  password: string;
+  preferences: string;
+  credit_card_id: string;
+}
